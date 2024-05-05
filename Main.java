2016240d79a0551032fee38272105e993fd7fc11
Demolka183 <166244786+Demolka183@users.noreply.github.com>
2024-05-05 11:53:32 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,39 @@
+import company.models.Manager;
+import company.models.Worker;
+import company.abstracts.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Worker worker1 = new Worker("Wiktor", 2000, 1, "2022-01-01", "QA Engineer");
+        Worker worker2 = new Worker("Oliwia", 1800, 2, "2021-12-15", "Project Manager");
+        Worker worker3 = new Worker("Jerzy", 2200, 3, "2022-02-10", "Senior IT Specialist");
+        Worker worker4 = new Worker("Filip", 1900, 4, "2022-03-05", "Junior IT Specialist");
+
+        Manager manager = new Manager("Krzysztof", 3500, 5, "2021-11-20", "Team Leader");
+
+        List<Employee> employees = new ArrayList<>();
+        employees.add(worker1);
+        employees.add(worker2);
+        employees.add(worker3);
+        employees.add(worker4);
+        employees.add(manager);
+
+        for (Employee employee : employees) {
+            System.out.println("- " + employee.getName() + " posiada kod: " + employee.hashCode());
+        }
+
+        System.out.println();
+
+        Worker worker5 = new Worker("Anna", 2500, 2, "2022-04-15", "Software Developer");
+        for (Employee employee : employees) {
+            if (worker5.equals(employee)) {
+                System.out.println("Pracownik " + worker5.getName() + " jest taki sam jak " + employee.getName());
+            } else {
+                System.out.println("Worker " + worker5.getName() + " nie jest taki sam jak " + employee.getName());
+            }
+        }
+    }
+}
